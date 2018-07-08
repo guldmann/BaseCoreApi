0531597fc89c4396a2f0431c103f1a10cd12c7d8
john larsen <john@pcis.se>
2018-07-08 17:44:05 +0200

    Init commit

---
+++ BaseCoreApi/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Swashbuckle;
+using Swashbuckle.AspNetCore.Swagger;
+//using Serilog.Sinks.LogstashHttp;
+
+namespace BaseCoreApi
+{
+    public class Startup
+    {
+        /*
+         *  TODO: Add a New Example controlle tha use
+         *  a Class that is built on a interface  example
+         *  
+         * 
+         * 
+        */
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                //.WriteTo.LogstashHttp("localhost:9600")
+                .WriteTo.RollingFile(Path.Combine(env.ContentRootPath, "BaseCoreApi-log-{Date}.log"))
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "BaseCoreApi API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog(); 
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BaseCoreApi API V1");
+            });            
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
