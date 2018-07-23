474dc4844dc76f079dd676630d9f5eed39b5f3dc
john larsen <john@pcis.se>
2018-07-23 15:12:46 +0200

    Added XML comments.

--- BaseCoreApi/Controllers/PingController.cs
+++ BaseCoreApi/Controllers/PingController.cs
@@ -5,7 +5,12 @@ namespace BaseCoreApi.Controllers
 {
     [Route("api/[controller]")]
     public class PingController : Controller
-    {       
+    {
+        /// <summary>
+        /// Heartbeat to test api for responsiveness  
+        /// </summary>
+        /// <returns>string "Pong"</returns>
+        /// <response code="200">String "Pong"</response> 
         [HttpGet]
         [HttpPost]
         public IActionResult Get()
